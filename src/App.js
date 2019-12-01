497ebad8e5c8b8bedc60d34736852d409c8fad48
Vanessa Randall <vanessalrandall@gmail.com>
2019-11-30 18:36:14 -0700

    Lay out bare bones html structure within App component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,51 @@ import { getAllAlbums } from './apiCalls/apiCalls';
 import './App.css';
 
 class App extends Component {
-  componentDidMount = async () => {
-    let albums = await getAllAlbums();
-    console.log('albums', albums);
-  };
+  constructor() {
+    super();
+    this.state = {
+      albums: [],
+      songs: [],
+      album: {},
+      song: {},
+      postedAlbum: {},
+      postedSong: {},
+      deletedAlbum: '',
+      deletedSong: ''
+    };
+  }
 
   render() {
-    return <div className='App'></div>;
+    return (
+      <div className='App'>
+        <h1 className='App__h1'>Welcome to The (unofficial) Beatles API!</h1>
+        <h5 className='App__h6--description'>Query strings and description</h5>
+        <div className='App__div--container'>
+          <h2>GET: <code>api/v1/albums</code></h2>
+          <div className='App__div--GET'>
+            <p>Hello</p>
+          </div>
+        </div>
+        <div className='App__div--container'>
+          <h2>GET: <code>api/v1/albums/:id</code></h2>
+          <div className='App__div--GET'>
+            <p>Hello</p>
+          </div>
+        </div>
+        <div className='App__div--container'>
+          <h2>GET: <code>api/v1/songs</code></h2>
+          <div className='App__div--GET'>
+            <p>Hello</p>
+          </div>
+        </div>
+        <div className='App__div--container'>
+          <h2>GET: <code>api/v1/songs/:id</code></h2>
+          <div className='App__div--GET'>
+            <p>Hello</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
