d82ef9d68e796092f854998256bdb56350c28846
Vanessa Randall <vanessalrandall@gmail.com>
2019-11-30 19:36:25 -0700

    Display all abums on click of GET button in first GET section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactJson from 'react-json-view';
 import { getAllAlbums } from './apiCalls/apiCalls';
 import './App.css';
 
@@ -17,33 +18,106 @@ class App extends Component {
     };
   }
 
+  getAlbums = async () => {
+    let albums = await getAllAlbums();
+    this.setState({ albums });
+  };
+
+  clearAlbums = () => {
+    this.setState({ albums: {} });
+  };
+
   render() {
     return (
       <div className='App'>
         <h1 className='App__h1'>Welcome to The (unofficial) Beatles API!</h1>
         <h5 className='App__h6--description'>Query strings and description</h5>
         <div className='App__div--container'>
-          <h2>GET: <code>api/v1/albums</code></h2>
-          <div className='App__div--GET'>
-            <p>Hello</p>
+          <h2 className='App__h2--endpoints'>
+            GET: <code>api/v1/albums</code>
+          </h2>
+          <div className='App__div--endpoints'>
+            <button
+              className='App__button--submit-to-endpoint'
+              onClick={this.getAlbums}
+            >
+              GET
+            </button>
+            <button
+              className='App__button--reset-request'
+              onClick={this.clearAlbums}
+            >
+              RESET
+            </button>
+            <ReactJson src={this.state.albums} theme='hopscotch' />
           </div>
         </div>
         <div className='App__div--container'>
-          <h2>GET: <code>api/v1/albums/:id</code></h2>
-          <div className='App__div--GET'>
-            <p>Hello</p>
+          <h2 className='App__h2--endpoints'>
+            GET: <code>api/v1/albums/:id</code>
+          </h2>
+          <div className='App__div--endpoints'>
+            <button className='App__button--submit-to-endpoint'>GET</button>
+            <button className='App__button--reset-request'>RESET</button>
+            <ReactJson src={this.state.album} theme='hopscotch' />
           </div>
         </div>
         <div className='App__div--container'>
-          <h2>GET: <code>api/v1/songs</code></h2>
-          <div className='App__div--GET'>
-            <p>Hello</p>
+          <h2 className='App__h2--endpoints'>
+            GET: <code>api/v1/songs</code>
+          </h2>
+          <div className='App__div--endpoints'>
+            <button className='App__button--submit-to-endpoint'>GET</button>
+            <button className='App__button--reset-request'>RESET</button>
+            <ReactJson src={this.state.songs} theme='hopscotch' />
           </div>
         </div>
         <div className='App__div--container'>
-          <h2>GET: <code>api/v1/songs/:id</code></h2>
-          <div className='App__div--GET'>
-            <p>Hello</p>
+          <h2 className='App__h2--endpoints'>
+            GET: <code>api/v1/songs/:id</code>
+          </h2>
+          <div className='App__div--endpoints'>
+            <button className='App__button--submit-to-endpoint'>GET</button>
+            <button className='App__button--reset-request'>RESET</button>
+            <ReactJson src={this.state.song} theme='hopscotch' />
+          </div>
+        </div>
+        <div className='App__div--container'>
+          <h2 className='App__h2--endpoints'>
+            POST: <code>api/v1/albums/:id</code>
+          </h2>
+          <div className='App__div--endpoints'>
+            <button className='App__button--submit-to-endpoint'>POST</button>
+            <button className='App__button--reset-request'>RESET</button>
+            <ReactJson src={this.state.postedAlbum} theme='hopscotch' />
+          </div>
+        </div>
+        <div className='App__div--container'>
+          <h2 className='App__h2--endpoints'>
+            POST: <code>api/v1/songs/:id</code>
+          </h2>
+          <div className='App__div--endpoints'>
+            <button className='App__button--submit-to-endpoint'>POST</button>
+            <button className='App__button--reset-request'>RESET</button>
+            <ReactJson src={this.state.postedSong} theme='hopscotch' />
+          </div>
+        </div>
+        <div className='App__div--container'>
+          <h2 className='App__h2--endpoints'>
+            DELETE: <code>api/v1/songs/:id</code>
+          </h2>
+          <div className='App__div--endpoints'>
+            <button className='App__button--submit-to-endpoint'>DELETE</button>
+            <button className='App__button--reset-request'>RESET</button>
+          </div>
+        </div>
+        <div className='App__div--container'>
+          <h2 className='App__h2--endpoints'>
+            DELETE: <code>api/v1/songs/:id</code>
+          </h2>
+          <div className='App__div--endpoints'>
+            <button className='App__button--submit-to-endpoint'>DELETE</button>
+            <button className='App__button--reset-request'>RESET</button>
           </div>
         </div>
       </div>
